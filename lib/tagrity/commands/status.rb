ffd266369d87be332d9a8981cc233bba405952a1
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-02 20:29:59 -0500

    Cleanup status

--- lib/tagrity/commands/status.rb
+++ lib/tagrity/commands/status.rb
@@ -5,21 +5,8 @@ module Tagrity
     class Status
       class << self
         def call
-          pid_files = PidFile.alive_pid_files.each do |pid_file|
-            pid_file
-          end
-
-          return if pid_files.empty?
-
-          max_pid_len = pid_files.max do |a, b|
-            a.pid.digits.count <=> b.pid.digits.count
-          end.pid.digits.count
-          max_dir_len = pid_files.max do |a, b|
-            a.dir.length <=> b.dir.length
-          end.dir.length
-
-          pid_files.each do |pid_file|
-            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid.to_s.ljust(max_pid_len)}}} {{green:#{pid_file.dir.ljust(max_dir_len)}}}"
+          PidFile.alive_pid_files.each do |pid_file|
+            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid}}} {{green:#{pid_file.dir}}}"
           end
         end
       end
