6513876467edfee992cd855929a47c96d83ca3ea
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-02 20:36:13 -0500

    Add some styling to status

--- lib/tagrity/commands/status.rb
+++ lib/tagrity/commands/status.rb
@@ -6,7 +6,8 @@ module Tagrity
       class << self
         def call
           PidFile.alive_pid_files.each do |pid_file|
-            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid}}} {{green:#{pid_file.dir}}}"
+            dir = pid_file.dir
+            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid}}} {{green:#{dir.split('/')[0,dir.length-1].join('/')}/}}{{magenta:#{dir.split('/')[-1]}}}"
           end
         end
       end
