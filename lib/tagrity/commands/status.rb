7248eb9d5aa8d897295ea7ecb3d6614448e6f3f3
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-08 13:03:27 -0500

    Bump to 0.2.5 and fix status again

--- lib/tagrity/commands/status.rb
+++ lib/tagrity/commands/status.rb
@@ -6,8 +6,8 @@ module Tagrity
       class << self
         def call
           PidFile.alive_pid_files.each do |pid_file|
-            dir = pid_file.dir
-            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid}}} {{green:#{dir.split('/')[0,dir.length-2].join('/')}/}}{{magenta:#{dir.split('/')[-1]}}}"
+            dir_a = pid_file.dir.split('/')
+            puts ::CLI::UI.fmt "{{cyan:#{pid_file.pid}}} {{green:#{dir_a[0,dir_a.length-1].join('/')}/}}{{magenta:#{dir_a[-1]}}}"
           end
         end
       end
